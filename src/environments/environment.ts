31fe96f204c3f7802e851cb651da9af09da09a5e
Digamber Singh <digamber@positronx.io>
2022-04-15 07:14:50 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'xxxxxxxx-xxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    databaseURL: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    projectId: 'xxxxxxxx',
+    storageBucket: 'xxxxxxxx',
+    messagingSenderId: 'xxxxxx',
+    appId: 'xxxxx',
+    measurementId: 'xxxxxxxxxxxxxxxx',
+  },
+};
